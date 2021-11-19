cc166462050167c50d67a069b1ba0ae74f3e038e
qkrtkdwns3410 <qkrtkdwns3410@naver.com>
2021-11-19 14:56:12 +0900

    채팅방 커밋

--- src/main/java/kr/or/ssff/studyIns/configuration/SocketConfiguration.java
+++ src/main/java/kr/or/ssff/studyIns/configuration/SocketConfiguration.java
@@ -15,14 +15,17 @@ public class SocketConfiguration extends AbstractWebSocketMessageBrokerConfigure
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
+        //메모리 기반 메세지 브로커가 해당 api를 구독하고 있는 클라이언트에게 메세지를 전달합니다.
         config.enableSimpleBroker("/topic");
+        //서버에서 클라이언트로부터의 메세지를 받을 api의 prefix(전치) 를 설정
         config.setApplicationDestinationPrefixes("/app");
     }
 
+    //클라이언트에서 WebSocket을 연결할 api를 설정합니다.
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/users");
-        registry.addEndpoint("/users").withSockJS();
+        //파라메터로 넘겨받는 StompEndpointRegistry 메소드인 addEndpoint() 메소드를 통해서 여러가지의 end point를 설정할 수 있습니다.
+        registry.addEndpoint("/websockethandler").withSockJS();
     }
 }
 
---
+++ src/main/java/kr/or/ssff/studyIns/controller/MessageHandler.java
@@ -0,0 +1,56 @@
+package kr.or.ssff.studyIns.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import kr.or.ssff.studyIns.model.ChatMsgDTO;
+import kr.or.ssff.studyIns.model.HelloMessage;
+import kr.or.ssff.studyIns.model.Message;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/*
+
+ */
+@RestController
+@Log4j2
+public class MessageHandler {
+
+    //@MessageMapping 어노텡션에 의해 /hello 라는 api로 매핑이 되어 있습니다.
+    //클라이언트에서 "../hello"라는 api로 메세제를 보내면  broadcasting 이라는 메서드가 호출됩니다
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/roomId")
+    public Message broadcasting(HelloMessage message) throws Exception {
+        return new Message(message.getContent());
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
